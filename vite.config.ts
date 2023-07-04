2ed3bd81251708ce54891bc27b826cc2cf03c4ac
inovue <inouve3@gmail.com>
2023-07-04 12:25:27 +0900

    add @types/node

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
+import {resolve} from 'path'
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/lib/index.jsx'),
+      name: 'My Component',
+      fileName: (format) => `my-component.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  },
   plugins: [react()],
 })
